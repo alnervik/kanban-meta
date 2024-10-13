f243bdddb3c3738dc5e6b91a40bcb9cf219e85a7
alnervik <k.alnervik@gmail.com>
2024-10-13 15:19:14 +0200

    edited fetch. added if-else loop and  log to verify that it works)

--- modules/verifyLogin.mjs
+++ modules/verifyLogin.mjs
@@ -1,7 +1,3 @@
-// Temporära värden för att testa inlogg
-const username = 'test';
-const password = '1234';
-
 //Funktion för att verifiera inloggningen
 export function verifyLogin() {
     //Hämtar värden från inputfältet
@@ -11,9 +7,20 @@ export function verifyLogin() {
     //Hämtar data från users.json
     fetch('./users.json')
     .then(res => res.json())
-    .then(users => {
+    .then(data => {
+        //Behöver få ut data.users för att kunna använda .find
+        const users = data.users;
+
         //Använder .find för att hitta användaren som matchar inmatade värden
-        const matchingUser = users.find(user =>
-            user.username == submittedUsername && user.password == submittedPassword);
-    })
+        const matchingUser = users.find(user => {
+            return user.username === submittedUsername && user.password === submittedPassword;
+        });
+            //Skriver ut i loggen om användaren finns eller inte
+            //(kommer fixas att du blir omdirigerad alt. felmeddelande i popup)
+            if (matchingUser) {
+                console.log('Inloggad som:', matchingUser.username);
+            } else {
+                console.log('Fel användarnamn eller lösenord');
+        }
+    });
 }
