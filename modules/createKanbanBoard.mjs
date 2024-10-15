e0a9fbc1d3109b5c42fc05aacfd404612e9cf9b1
Vilhelm Fontell <ville@fontell.se>
2024-10-15 15:33:52 +0200

    Fixa knapp för att lägga till nytt kort (issue #31)

--- modules/createKanbanBoard.mjs
+++ modules/createKanbanBoard.mjs
@@ -2,8 +2,8 @@ import { createNewElement } from './createNewElement.mjs';
 import { createCard } from './createCardScript.mjs';
 
 export function createKanbanBoard() {
-   //Ändrade till root då vi har en root i index.html
-   //men skapar mainContent sedan för att kunna placera den i root och framöver placera resterande element i mainContent
+   // Ändrade till root då vi har en root i index.html
+   // men skapar mainContent sedan för att kunna placera den i root och framöver placera resterande element i mainContent
    const root = document.getElementById('root');
    root.innerHTML = '';
    const mainContent = createNewElement('div', null, 'mainContent', null, root);
@@ -18,32 +18,18 @@ export function createKanbanBoard() {
    // Går igenom varje kolumn och lägger till ett tomt kort
    const columnsArray = [todo, doing, testing, done];
    columnsArray.forEach(column => {
-      column.appendChild(createCard());
-      addButtonListener(column);
-   });
-
-   const textarea = document.querySelector('textarea');
-
-
-
-   // Detta körs i en egen funktion är för att column ändrar värde och måste skickas in som en parameter för att behålla sitt rätta värde
-   function addButtonListener(column) {
-      // Hämtar det sista elementet i DOM:en med klassen addCard och lägger till eventListenern på det
-      const numberOfCards = column.getElementsByClassName('addCard').length;
-      const lastCard = column.getElementsByClassName('addCard')[numberOfCards - 1];
-      lastCard.onclick = () => {
-         if (lastCard.innerText !== 'Save changes') {
-            column.appendChild(createCard());
-            // Kör funktionen igen för att lägga till eventListener på det nyskapade kortet
-            addButtonListener(column);
-         }
+      const columnText = column.innerText;
+      column.innerText = null;
+      const titleContainer = createNewElement('div', columnText, 'titleContainer', 'titleContainer', column);
+      const addCardButton = createNewElement('button', '+', 'addCardButton', 'addCardButton', titleContainer);
+      addCardButton.onclick = () => {
+         column.appendChild(createCard());
+         // Gör så att textrutans storlek anpassar sig till innehållet
+         const textarea = column.querySelector('textarea');
+         textarea.addEventListener('input', () => {
+            textarea.style.height = 'auto';
+            textarea.style.height = (textarea.scrollHeight) + 'px';
+         });
       };
-
-      // Gör så att textrutans storlek anpassar sig till innehållet
-      const textarea = lastCard.parentElement.querySelector('textarea');
-      textarea.addEventListener('input', () => {
-         textarea.style.height = 'auto';
-         textarea.style.height = (textarea.scrollHeight) + 'px';
-      });
-   }
+   });
 }
